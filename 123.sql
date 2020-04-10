18ef5f1fd0d8b6c448dbb2cdb5ac3cf12cbe9467
1937425001 <50541341+1937425001@users.noreply.github.com>
2020-04-10 21:00:08 +0800

    dsa
    asdfasf

---
+++ 123.sql
@@ -0,0 +1,9 @@
+select t1.id, t1.`name`,t.id ,t.name 
+from yzs_unified_app tleft join yzs_unified_app_group t1
+on t.group_id = t1.id;
+
+
+-- 场景配置
+select t.id themeId,t.name,t2.id appid,t2.name from yzs_unified_theme t
+left join yzs_unified_app_theme  t1 on t.id =t1.theme_id
+left join yzs_unified_app t2 on t1.app_id=t2.id
